6a185ef486029297163f15d4d242144b693e1303
IT23684508DRW <IT23684508@my.sliit.lk>
2025-09-24 09:50:25 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT 23684508 Lab 06/IT23684508.R
@@ -0,0 +1,23 @@
+setwd("C:\\Users\\USER\\Desktop\\IT 23684508 Lab 06")
+
+#Exercise
+#01
+
+#i
+#Binomial Distribution
+#Here, Random veriable X has Binomial Distribution with n = 50 and p = 0.85 
+
+#ii
+pbinom(47,50,0.85,lower.tail = FALSE)
+
+#02
+
+#i
+#Customer calls per hour
+
+#ii
+#Poisson Distribution
+#Here, Random veriable X has Poisson Distribution with lamda = 12 
+
+#iii
+dpois(15,12)
